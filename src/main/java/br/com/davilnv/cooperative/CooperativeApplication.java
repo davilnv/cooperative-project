f42ba50413aff453d8bcd4bd8035971c366251cc
davilnv <davilima871@gmail.com>
2024-10-17 22:38:31 -0300

    Create README.md

--- src/main/java/br/com/davilnv/cooperative/CooperativeApplication.java
+++ src/main/java/br/com/davilnv/cooperative/CooperativeApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CooperativeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CooperativeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CooperativeApplication.class, args);
+    }
 
 }
