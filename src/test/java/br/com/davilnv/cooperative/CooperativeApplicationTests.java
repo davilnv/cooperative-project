2bb00aa19f12d033ccaff555a5ad41880d5dbdd6
davilnv <davilima871@gmail.com>
2024-10-17 22:37:36 -0300

    Initial commit

---
+++ src/test/java/br/com/davilnv/cooperative/CooperativeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.davilnv.cooperative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CooperativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
