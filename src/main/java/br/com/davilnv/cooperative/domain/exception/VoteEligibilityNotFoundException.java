2887edaa28312f8c945a36d97543b4852664cd0f
davilnv <davilima871@gmail.com>
2024-10-21 17:12:52 -0300

    Feat: API Rest Service for check user is ABLE_TO_VOTE or UNABLE_TO_VOTE

---
+++ src/main/java/br/com/davilnv/cooperative/domain/exception/VoteEligibilityNotFoundException.java
@@ -0,0 +1,7 @@
+package br.com.davilnv.cooperative.domain.exception;
+
+public class VoteEligibilityNotFoundException extends Exception {
+    public VoteEligibilityNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/br/com/davilnv/cooperative/domain/exception/VoteEligibilitySystemException.java
@@ -0,0 +1,7 @@
+package br.com.davilnv.cooperative.domain.exception;
+
+public class VoteEligibilitySystemException extends Exception {
+    public VoteEligibilitySystemException(String message) {
+        super(message);
+    }
+}
