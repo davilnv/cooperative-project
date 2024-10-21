675581ec0c963e8816092d60145c1210f2599fe7
davilnv <davilima871@gmail.com>
2024-10-21 12:03:41 -0300

    Fix: Tests breaks without postgres database, change to use test profile properties

--- src/test/java/br/com/davilnv/cooperative/CooperativeApplicationTests.java
+++ src/test/java/br/com/davilnv/cooperative/CooperativeApplicationTests.java
@@ -2,8 +2,10 @@ package br.com.davilnv.cooperative;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CooperativeApplicationTests {
 
     @Test
